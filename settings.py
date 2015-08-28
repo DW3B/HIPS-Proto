3cccb75b594d6a113ecdf23f5bc837f01f72f780
unknown <dwebster@US2M5C5R1NB.CI.ORG>
2015-08-28 15:21:56 -0600

    Things and stuff

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 '''
- 	HIPSproto.py settings
-	v0.1
+HIPSproto.py settings
+v0.1
 
-	Visit http://api.imgur.com/ to register an application and
-	receive your client_id and client_secret. If this script 
-	is the only thing you will use these for choose "Anonymous
-	usage without user authorization" on your registration.  
+visit http://api.imgur.com/ to register an application and
+receive your client_id and client_secret. If this script 
+is the only thing you will use these for choose "Anonymous
+usage without user authorization" on your registration.  
 '''
 
 settings = {
